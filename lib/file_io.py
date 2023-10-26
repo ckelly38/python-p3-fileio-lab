7be03cb791543c85d284a55848fc21ebb29c0ebb
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-26 03:23:49 -0600

    this an example Python FileIO lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,31 @@
 def write_file(file_name, file_content):
-    pass
+    myfile = None;
+    try:
+        myfile = open(str(file_name) + ".txt", mode="w", encoding="utf-8");
+        myfile.write(file_content);
+        myfile.close();
+    finally:
+        if (myfile == None): pass;
+        else: myfile.close();
 
 def append_file(file_name, append_content):
-    pass
+    myfile = None;
+    try:
+        myfile = open(str(file_name) + ".txt", mode="a", encoding="utf-8");
+        myfile.write(append_content);
+        myfile.close();
+    finally:
+        if (myfile == None): pass;
+        else: myfile.close();
 
 def read_file(file_name):
-    pass
+    myfile = None;
+    mycontent = None;
+    try:
+        myfile = open(str(file_name) + ".txt", mode="r", encoding="utf-8");
+        mycontent = myfile.read();
+        myfile.close();
+    finally:
+        if (myfile == None): pass;
+        else: myfile.close();
+    return mycontent;
